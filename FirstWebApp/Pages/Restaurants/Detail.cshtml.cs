6e25b7591880dd9fceb9591e23ce937b4ea8c9d2
Marco Vavassori <mvavassori@abacusnext.com>
2022-09-29 15:41:35 +0100

    Introduce create feature and add "Restaurant saved!" UI message

--- FirstWebApp/Pages/Restaurants/Detail.cshtml.cs
+++ FirstWebApp/Pages/Restaurants/Detail.cshtml.cs
@@ -7,8 +7,11 @@ namespace FirstWebApp.Pages.Restaurants
 {
     public class DetailModel : PageModel
     {
+        private readonly IRestaurantData restaurantData;
+
+        [TempData]
+        public string Message { get; set; }
         public Restaurant Restaurant { get; set; }
-        public IRestaurantData restaurantData;
 
         public DetailModel(IRestaurantData restaurantData)
         {
